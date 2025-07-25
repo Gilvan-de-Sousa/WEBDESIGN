69323dc7b10978378887f89070b77f1e53084cc9
Gilvan-de-Sousa <122160451+Gilvan-de-Sousa@users.noreply.github.com>
2025-07-25 17:45:49 -0300

    js aula05

--- estudo_javascript/aula04/aula01.js
+++ estudo_javascript/aula04/aula01.js
@@ -1,6 +1,8 @@
-// Comentário
-// Comentário
-// Comentário
-// Comentário
-// Comentário
+
 console.log("JavaScript primeiros passos!")
+
+// window.alert("Script rodando!")
+
+// window.confirm("Está gostando do curso?")
+
+// window.prompt("Qual o seu nome: ")

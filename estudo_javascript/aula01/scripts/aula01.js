a7b144fd8dd73756fb28f5cb9f885bd1a93be7a6
Gilvan-de-Sousa <122160451+Gilvan-de-Sousa@users.noreply.github.com>
2025-07-25 11:04:56 -0300

    ex001 - JavaScript

--- estudo_javascript/aula01/scripts/aula01.js
+++ estudo_javascript/aula01/scripts/aula01.js
@@ -5,4 +5,17 @@
 // Comentário
 console.log("JavaScript primeiros passos!")
 
-// document.body.style.background = "black";
+// document.body.style.backgroundColor = "darkgreen";
+
+document.body.onmouseover = function(){
+    this.style.backgroundColor = "darkgray";
+
+}
+
+document.body.onmouseout = function(){
+    this.style.backgroundColor = "brown";
+}
+
+// document.body.onmouseover = function(){
+//     this.style.color = "black";
+// }

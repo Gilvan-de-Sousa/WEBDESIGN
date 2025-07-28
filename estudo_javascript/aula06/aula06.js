86844f71604f7d06a6512807b5d8abbe00a1f047
Gilvan-de-Sousa <122160451+Gilvan-de-Sousa@users.noreply.github.com>
2025-07-28 16:48:38 -0300

    aula07

---
+++ estudo_javascript/aula06/aula06.js
@@ -0,0 +1,11 @@
+// Tratamento de Dados
+
+console.log("JavaScript primeiros passos!")
+
+//Guardando os dados em uma variável
+var nome = window.prompt("Qual é o seu nome: ")
+
+//Exibindo o dado armazenado com o alert
+//Template string
+var s = "JavaScript"
+window.alert(`Eu estou aprendendo ${s}`)

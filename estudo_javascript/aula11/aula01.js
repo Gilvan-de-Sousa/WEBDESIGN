1f96c60251abe89e20fadafd3f0f3674ba945ca3
Gilvan-de-Sousa <122160451+Gilvan-de-Sousa@users.noreply.github.com>
2025-08-04 14:12:48 -0300

    ex010 javascript

--- estudo_javascript/aula11/aula01.js
+++
@@ -1,6 +0,0 @@
-// Comentário
-// Comentário
-// Comentário
-// Comentário
-// Comentário
-console.log("JavaScript primeiros passos!")
---
+++ estudo_javascript/aula11/ex008.js
@@ -0,0 +1,15 @@
+// Comentário
+// Comentário
+// Comentário
+// Comentário
+// Comentário
+var vel = 60.5
+// console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
+// console.log("Sempre use cinto de segurança!")
+
+if(vel > 70){
+    console.log(`A sua velocidade é de ${vel} Km/h, você está acima no limite da via`)
+}else{
+    console.log(`Você está dentro do limite de segurança: ${vel} Km/h`)
+}
+console.log("Sempre use cinto de segurança!")

0c5c4cf303d5a5a6990bc3505408a43803970e88
Gilvan-de-Sousa <122160451+Gilvan-de-Sousa@users.noreply.github.com>
2025-08-15 17:35:18 -0300

    fim do exercicio 18

--- estudo_javascript/aula16_ex/ex018/script_ex18.js
+++ estudo_javascript/aula16_ex/ex018/script_ex18.js
@@ -12,7 +12,7 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-  if (l.indexOf(Number(n) != -1)) {
+  if (l.indexOf(Number(n)) != -1) {
     return true;
   } else {
     return false;
@@ -21,8 +21,44 @@ function inLista(n, l) {
 
 function adicionar() {
   if (isNumero(num.value) && !inLista(num.value, valores)) {
-    alert("Tudo ok!");
+    valores.push(Number(num.value))
+
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} ..... adicionado`
+    lista.appendChild(item)
+    res.innerHTML = " "
   } else {
     window.alert("Valor inválido ou já existente na lista!");
   }
+
+  num.value = " "
+  num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert("Adicione valores antes de finalizar!")
+    }else{
+
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let i in valores){
+            soma += valores[i]
+            media += valores[i] / valores.length
+            if(valores[i] > maior)
+                maior = valores[i]
+
+            if(valores[i] < menor)
+                menor = valores[i]
+        }
+        res.innerHTML = " "
+        res.innerHTML += `Ao todo temos ${tot} números cadastrados!`
+        res.innerHTML += `\n<br>O maior valor informado foi ${maior}.`
+        res.innerHTML += `\n<br>O menor valor informado foi ${menor}.`
+        res.innerHTML += `\n<br>A soma de todos os valores é: ${soma}`
+        res.innerHTML += `\n<br>O valor da média dos valores é ${media}`
+    }
 }
